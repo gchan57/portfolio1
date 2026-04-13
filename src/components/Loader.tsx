98ba7e0ad105d3beea3d3fa762c1342489df2d0d
Gunachandru <tgunachandru@gmail.com>
2026-04-13 14:10:11 +0530

    addedloading

---
+++ src/components/Loader.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Loader = () => {
+  return (
+    <StyledWrapper>
+      <div className="newtons-cradle">
+        <div className="newtons-cradle__dot" />
+        <div className="newtons-cradle__dot" />
+        <div className="newtons-cradle__dot" />
+        <div className="newtons-cradle__dot" />
+      </div>
+    </StyledWrapper>
+  );
+}
+
+const StyledWrapper = styled.div`
+  /* Center the loader on the screen */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: white; /* Match your site's background */
+  z-index: 9999;
+
+  .newtons-cradle {
+   --uib-size: 50px;
+   --uib-speed: 1.2s;
+   --uib-color: #474554;
+   position: relative;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   width: var(--uib-size);
+   height: var(--uib-size);
+  }
+
+  .newtons-cradle__dot {
+   position: relative;
+   display: flex;
+   align-items: center;
+   height: 100%;
+   width: 25%;
+   transform-origin: center top;
+  }
+
+  .newtons-cradle__dot::after {
+   content: '';
+   display: block;
+   width: 100%;
+   height: 25%;
+   border-radius: 50%;
+   background-color: var(--uib-color);
+  }
+
+  .newtons-cradle__dot:first-child {
+   animation: swing var(--uib-speed) linear infinite;
+  }
+
+  .newtons-cradle__dot:last-child {
+   animation: swing2 var(--uib-speed) linear infinite;
+  }
+
+  @keyframes swing {
+   0% { transform: rotate(0deg); animation-timing-function: ease-out; }
+   25% { transform: rotate(70deg); animation-timing-function: ease-in; }
+   50% { transform: rotate(0deg); animation-timing-function: linear; }
+  }
+
+  @keyframes swing2 {
+   0% { transform: rotate(0deg); animation-timing-function: linear; }
+   50% { transform: rotate(0deg); animation-timing-function: ease-out; }
+   75% { transform: rotate(-70deg); animation-timing-function: ease-in; }
+  }`;
+
+export default Loader;
